7f81bc657a64de6d66c9d6b8ff30043ef1f3518e
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2025-03-03 08:56:47 +0100

    Working test

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,42 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new Item("foo", 0, 2) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void cassoulet() {
+        Item[] items = new Item[]{
+                new Item(GildedRose.AGED_BRIE, 5, 10),
+                new Item(GildedRose.SULFURAS_HAND_OF_RAGNAROS, 0, 80),
+                new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 4, 1),
+                new Item("Patate", 1, 1),
+                new Item("Patate",0,1),
+                new Item(GildedRose.BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT, 0, 10),
+                new Item(GildedRose.AGED_BRIE, 0, 10)
+        };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(80, app.items[1].quality);
+        assertEquals(0, app.items[1].sellIn);
+        assertEquals(4, app.items[2].quality);
+        assertEquals(3, app.items[2].sellIn);
+        assertEquals(0, app.items[3].quality);
+        assertEquals(0, app.items[3].sellIn);
+        assertEquals(0, app.items[4].quality);
+        assertEquals(-1, app.items[4].sellIn);
+        assertEquals(0, app.items[5].quality);
+        assertEquals(-1, app.items[5].sellIn);
+        assertEquals(12, app.items[6].quality);
+        assertEquals(-1, app.items[6].sellIn);
+
+
+
     }
 
 }
