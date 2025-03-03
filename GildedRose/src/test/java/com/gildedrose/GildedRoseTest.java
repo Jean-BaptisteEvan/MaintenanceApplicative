a8aae5cf3c121051af034175b897ac580782507b
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2025-03-03 10:53:04 +0100

    100% branch coverage

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -21,9 +21,13 @@ class GildedRoseTest {
                 new Item(GildedRose.SULFURAS, 0, 80),
                 new Item(GildedRose.CONCERT, 4, 1),
                 new Item("Patate", 1, 1),
-                new Item("Patate",0,1),
+                new Item("Patate",0,0),
                 new Item(GildedRose.CONCERT, 0, 10),
                 new Item(GildedRose.BRIE, 0, 10),
+                new Item(GildedRose.CONCERT, 30, 50),
+                new Item(GildedRose.CONCERT, 30, 40),
+                new Item(GildedRose.BRIE, 0, 50),
+
         };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -41,17 +45,18 @@ class GildedRoseTest {
         assertEquals(-1, app.items[5].sellIn);
         assertEquals(12, app.items[6].quality);
         assertEquals(-1, app.items[6].sellIn);
+        assertEquals(50, app.items[7].quality);
+        assertEquals(29, app.items[7].sellIn);
+        assertEquals(41, app.items[8].quality);
+        assertEquals(29, app.items[8].sellIn);
     }
 
     @Test
     void conjured(){
         Item[] items = new Item[]{
-                new Item(GildedRose.CONJURED, 2, 10)
+                new Item(GildedRose.CONJURED, 1, 8)
         };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(8, app.items[0].quality);
-        assertEquals(1, app.items[0].sellIn);
         app.updateQuality();
         assertEquals(6, app.items[0].quality);
         assertEquals(0, app.items[0].sellIn);
@@ -64,5 +69,11 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(0, app.items[0].quality);
         assertEquals(-3, app.items[0].sellIn);
+    }
+
+    @Test
+    void StringItem(){
+        Item i = new Item("foo", 0, 2);
+        assertEquals("foo, 0, 2",i.toString());
     }
 }
