417327da50842d0d7e6aa58ecdee3bcf4bd37811
Jean-Baptiste Evan~ <evanjeanbaptiste@gmail.com>
2025-03-03 10:40:50 +0100

    Adding conjured logic

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,7 +1,5 @@
 package com.gildedrose;
 
-import java.util.Objects;
-
 class GildedRose {
     public static final String BRIE = "Aged Brie";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
@@ -13,7 +11,7 @@ class GildedRose {
         this.items = items;
     }
 
-    public void updateQuality() {
+    public void updateQuality(){
         for (Item item : items) {
             handleItems(item);
         }
@@ -29,6 +27,9 @@ class GildedRose {
                 break;
             case SULFURAS:
                 //Nothing Happen
+                break;
+            case CONJURED:
+                handleConjured(item);
                 break;
             default:
                 handleOthers(item);
@@ -46,13 +47,13 @@ class GildedRose {
     }
 
     public void handleConcert(Item item){
+        item.sellIn = item.sellIn - 1;
         if (item.quality < 50 && item.sellIn >= 0) {
             item.quality++;
 
             if (item.sellIn < 11) {
                 item.quality++;
             }
-
             if (item.sellIn < 6) {
                 item.quality++;
             }
@@ -61,13 +62,23 @@ class GildedRose {
         }
     }
 
-    public void handleOthers(Item item){
+    public void handleConjured(Item item){
         if (item.quality > 0) {
-            item.quality = item.quality - 1;
+            item.quality = item.quality - 2;
         }
         item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0 && item.quality > 0) {
-                item.quality = item.quality - 1;
+            item.quality = item.quality - 2;
+        }
+    }
+
+    public void handleOthers(Item item){
+        if (item.quality > 0) {
+            item.quality--;
+        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0 && item.quality > 0) {
+                item.quality--;
         }
     }
 }
